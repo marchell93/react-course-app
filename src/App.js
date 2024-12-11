90f3e2624d63239650d348ad9d3b6fc2bbe34b94
Mark P. <marchell93@mail.ru>
2024-12-11 12:31:06 +0300

    Добавили форму для заполнения

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
 
 const App = () => {
@@ -21,7 +22,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Начнём изучение React</h1>
+      <NewCost />
       <Costs costs={costs} />
     </div>
   );
