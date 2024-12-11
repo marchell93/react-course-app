145296da17d9f3158885035cdfa5ed51cca2920f
Mark P. <marchell93@mail.ru>
2024-12-11 10:25:29 +0300

    Добавил в проект состояние (useState)

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,14 +1,23 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
+import React, { useState } from "react";
 
 const CostItem = (props) => {
+  const [description, setDescription] = useState(props.description);
+
+  function changeDescriptionHandler() {
+    setDescription("Новое описание");
+    console.log(description);
+  }
+
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
       <div className="cost-item__description">
-        <h2>{props.description}</h2>
+        <h2>{description}</h2>
         <div className="cost-item__price">${props.amount}</div>
+        <button onClick={changeDescriptionHandler}>Изменить описание</button>
       </div>
     </Card>
   );
