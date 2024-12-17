9cc6e181adc6f32f407265da24e131cfa497da7f
Mark P. <marchell93@mail.ru>
2024-12-17 17:02:50 +0300

    Прокинуть наверх данные через пропсы

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import Costs from "./components/Costs/Costs";
 const App = () => {
   const costs = [
     {
+      id: "c1",
       date: new Date(2021, 2, 12),
       description: "Холодильник",
       amount: 999.99,
     },
     {
+      id: "c2",
       date: new Date(2021, 11, 25),
       description: "MacBook",
       amount: 1254.72,
     },
     {
+      id: "c3",
       date: new Date(2021, 3, 1),
       description: "Джинсы",
       amount: 49.99,
     },
   ];
 
+  const addCostHandler = (cost) => {
+    console.log("In App.js");
+    console.log(cost);
+  };
+
   return (
     <div>
-      <NewCost />
+      <NewCost onAddCost={addCostHandler} />
       <Costs costs={costs} />
     </div>
   );
