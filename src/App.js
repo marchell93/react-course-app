5128e17999d230461fe684a0c87e182178e24320
Mark P. <marchell93@mail.ru>
2024-12-24 21:19:32 +0300

    Динамическое отображение карточек

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
+import React, { useState } from "react";
+
+const INITIAL_COST = [
+  {
+    id: "c1",
+    date: new Date(2021, 2, 12),
+    description: "Холодильник",
+    amount: 999.99,
+  },
+  {
+    id: "c2",
+    date: new Date(2021, 11, 25),
+    description: "MacBook",
+    amount: 1254.72,
+  },
+  {
+    id: "c3",
+    date: new Date(2021, 3, 1),
+    description: "Джинсы",
+    amount: 49.99,
+  },
+];
 
 const App = () => {
-  const costs = [
-    {
-      id: "c1",
-      date: new Date(2021, 2, 12),
-      description: "Холодильник",
-      amount: 999.99,
-    },
-    {
-      id: "c2",
-      date: new Date(2021, 11, 25),
-      description: "MacBook",
-      amount: 1254.72,
-    },
-    {
-      id: "c3",
-      date: new Date(2021, 3, 1),
-      description: "Джинсы",
-      amount: 49.99,
-    },
-  ];
+  const [costs, setCosts] = useState(INITIAL_COST);
 
   const addCostHandler = (cost) => {
-    console.log("In App.js");
-    console.log(cost);
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
+    });
   };
 
   return (
