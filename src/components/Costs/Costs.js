ead4c298afb9d7c2def5adabb730513ea2fa01b8
Mark P. <marchell93@mail.ru>
2024-12-24 16:10:38 +0300

    Решение задания 2

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,28 +1,39 @@
 import CostItem from "./CostItem";
 import "./Costs.css";
 import Card from "../UI/Card";
+import CostsFilter from "./CostsFilter";
+import React, { useState } from "react";
 
 const Costs = (props) => {
   const costs = props.costs;
 
+  const [selectedYear, setSelectedYear] = useState("2021");
+
+  const yearChangeHandler = (year) => {
+    setSelectedYear(year);
+  };
+
   return (
-    <Card className="costs">
-      <CostItem
-        date={costs[0].date}
-        description={costs[0].description}
-        amount={costs[0].amount}
-      />
-      <CostItem
-        date={costs[1].date}
-        description={costs[1].description}
-        amount={costs[1].amount}
-      />
-      <CostItem
-        date={costs[2].date}
-        description={costs[2].description}
-        amount={costs[2].amount}
-      />
-    </Card>
+    <div>
+      <Card className="costs">
+        <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
+        <CostItem
+          date={costs[0].date}
+          description={costs[0].description}
+          amount={costs[0].amount}
+        />
+        <CostItem
+          date={costs[1].date}
+          description={costs[1].description}
+          amount={costs[1].amount}
+        />
+        <CostItem
+          date={costs[2].date}
+          description={costs[2].description}
+          amount={costs[2].amount}
+        />
+      </Card>
+    </div>
   );
 };
 
