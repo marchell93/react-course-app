64d0e79c4ff60f8e830001916016065415612972
Mark <marchell93@mail.ru>
2024-12-27 22:48:06 +0300

    Создать новый компонент CostList

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -12,14 +12,16 @@ const CostItem = (props) => {
   }
 
   return (
-    <Card className="cost-item">
-      <CostDate date={props.date} />
-      <div className="cost-item__description">
-        <h2>{description}</h2>
-        <div className="cost-item__price">${props.amount}</div>
-        <button onClick={changeDescriptionHandler}>Изменить описание</button>
-      </div>
-    </Card>
+    <li>
+      <Card className="cost-item">
+        <CostDate date={props.date} />
+        <div className="cost-item__description">
+          <h2>{description}</h2>
+          <div className="cost-item__price">${props.amount}</div>
+          <button onClick={changeDescriptionHandler}>Изменить описание</button>
+        </div>
+      </Card>
+    </li>
   );
 };
 
--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,8 +1,8 @@
-import CostItem from "./CostItem";
 import "./Costs.css";
 import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
 import React, { useState } from "react";
+import CostList from "./CostList";
 
 const Costs = (props) => {
   const [selectedYear, setSelectedYear] = useState("2021");
@@ -15,24 +15,11 @@ const Costs = (props) => {
     (cost) => cost.date.getFullYear().toString() === selectedYear
   );
 
-  let contentCosts = <p>Нет расходов в этом году</p>;
-
-  if (filteredCosts.length > 0) {
-    contentCosts = filteredCosts.map((cost) => (
-      <CostItem
-        key={cost.id}
-        date={cost.date}
-        description={cost.description}
-        amount={cost.amount}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-        {contentCosts}
+        <CostList costs={filteredCosts} />
       </Card>
     </div>
   );
